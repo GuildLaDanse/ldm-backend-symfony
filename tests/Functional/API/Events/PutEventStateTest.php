18328235f7dcb03483f42e6213deaaa6a92c2926
Bavo De Ridder <bavo@bavoderidder.com>
2020-04-22 23:15:04 +0200

    Skeleton tests for Event API

---
+++ tests/Functional/API/Events/PutEventStateTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/**
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     https://github.com/GuildLaDanse
+ */
+
+namespace App\Tests\Functional\API\Events;
+
+
+use App\Tests\Functional\API\AbstractWebTestCase;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+class PutEventStateTest extends AbstractWebTestCase
+{
+    use FixturesTrait;
+
+    /** @var KernelBrowser|null  */
+    private ?KernelBrowser $client = null;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->client->followRedirects(true);
+    }
+}
